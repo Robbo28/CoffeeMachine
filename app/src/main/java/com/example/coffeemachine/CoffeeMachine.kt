e03747a9cdfa5994047e469e49ee5af943d8ef3b
user-zbBDC8prSfiKiZhcQVUjHQ <robert.haley@legrand.com>
2024-08-23 14:48:35 +0100

    Added data class

--- app/src/main/java/com/example/coffeemachine/CoffeeMachine.kt
+++ app/src/main/java/com/example/coffeemachine/CoffeeMachine.kt
@@ -1,22 +1,31 @@
 package com.example.coffeemachine
 
+data class CoffeeDetails(
+    val name : String,
+    val sugarCount : Int,
+    val size : String,
+    val creamAmount : Int
+)
+
 fun main(){
 
-    println("Who is the coffee for?")
+    val coffeeForDenis = CoffeeDetails("Denis", 2, "xxl", 0)
+
+    /*println("Who is the coffee for?")
     val name = readln()
 
     println("How many sugars would you like?")
     val sugarCount = readln()
-    val sugarCountInt = sugarCount.toInt()
+    val sugarCountInt = sugarCount.toInt()*/
 
-    makeCoffee(name, sugarCountInt)
+    makeCoffee(coffeeForDenis)
 }
 
-fun makeCoffee(name : String, sugarCount : Int){
+fun makeCoffee(coffeeDetails : CoffeeDetails){
     when {
-        sugarCount < 0 -> println("Invalid input: sugar count cannot be negative. Try again $name")
-        sugarCount == 0 -> println("Coffee with no sugar for $name")
-        sugarCount == 1 -> println("Coffee with $sugarCount spoon of sugar for $name")
-        else -> println("Coffee with $sugarCount spoons of sugar for $name")
+        coffeeDetails.sugarCount < 0 -> println("Invalid input: sugar count cannot be negative. Try again ${coffeeDetails.name}")
+        coffeeDetails.sugarCount == 0 -> println("Coffee with no sugar for ${coffeeDetails.name}")
+        coffeeDetails.sugarCount == 1 -> println("Coffee with ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.name}")
+        else -> println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name}")
     }
 }
